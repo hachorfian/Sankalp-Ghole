0df5c85b80a9d2a92522e0af29f4ec8e833e24bb
Sankalp Ghole <122429370+hachorfian@users.noreply.github.com>
2023-01-20 17:40:58 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,res;
+    printf("enter first number\n");
+    scanf("%d",&a);
+    printf("enter second number\n");
+    scanf("%d",&b);
+    res=a+b;
+    printf("result is:%d",res);
+    
+    return 0;
+    }
